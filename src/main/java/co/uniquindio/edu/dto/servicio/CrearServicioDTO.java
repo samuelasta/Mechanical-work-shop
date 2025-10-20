1a67f5657e45e6c0aa8ffa581ed2094af8162b98
samuelasta <samuela.castanom@uqvirtual.edu.co>
2025-10-20 08:30:00 +0200

    servicio

---
+++ src/main/java/co/uniquindio/edu/dto/servicio/CrearServicioDTO.java
@@ -0,0 +1,4 @@
+package co.uniquindio.edu.dto.servicio;
+
+public record CrearServicioDTO() {
+}
---
+++ src/main/java/co/uniquindio/edu/dto/servicio/ObtenerServicioDTO.java
@@ -0,0 +1,6 @@
+package co.uniquindio.edu.dto.servicio;
+
+import co.uniquindio.edu.model.enums.TipoServicio;
+
+public record ObtenerServicioDTO(TipoServicio tipoServicio) {
+}
